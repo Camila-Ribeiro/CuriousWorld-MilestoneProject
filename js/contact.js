b81f0a5ce4a808bad63b3cc58cbc0e99f45944ca
Camila <camilarf@gmail.com>
2020-03-26 17:17:43 +0000

    Implement emailjs integration and edited form on contact page

---
+++ js/contact.js
@@ -0,0 +1,16 @@
+function sendMail(contactForm) {
+    emailjs.send("gmail", "googleplaces_milestoneproject", {
+        "from_name": contactForm.fullname.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.textarea.value
+      })
+      .then(
+        function(response) {
+          console.log("SUCCESS", response);
+        },
+        function(error) {
+          console.log("FAILED", response);
+        }
+      );
+    return false;
+  }
