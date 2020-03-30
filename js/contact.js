7b299f71d4afa358d4fad88b96e5ab7ad41493d1
Camila <camilarf@gmail.com>
2020-03-30 12:16:00 +0100

    added main.js Google Places API - WIP

--- js/contact.js
+++ js/contact.js
@@ -1,16 +1,17 @@
 function sendMail(contactForm) {
-    emailjs.send("gmail", "googleplaces_milestoneproject", {
-        "from_name": contactForm.fullname.value,
-        "from_email": contactForm.emailaddress.value,
-        "project_request": contactForm.textarea.value
-      })
-      .then(
-        function(response) {
-          console.log("SUCCESS", response);
-        },
-        function(error) {
-          console.log("FAILED", response);
-        }
-      );
-    return false;
-  }
+  emailjs
+    .send("gmail", "googleplaces_milestoneproject", {
+      from_name: contactForm.fullname.value,
+      from_email: contactForm.emailaddress.value,
+      project_request: contactForm.textarea.value
+    })
+    .then(
+      function(response) {
+        console.log("SUCCESS", response);
+      },
+      function(error) {
+        console.log("FAILED", response);
+      }
+    );
+  return false;
+}
